8fefbb0b7a8392ca2997e4663ae8310d4990ee03
jin-daram <jindaram1999@gmail.com>
2026-04-22 23:04:15 +0900

    fest: 공통 예외 추가

--- common/src/main/java/com/heartbloom/be/common/exception/ServiceException.java
+++ common/src/main/java/com/heartbloom/be/common/exception/ServiceException.java
@@ -1,4 +1,21 @@
 package com.heartbloom.be.common.exception;
 
-public class ServiceException {
+public class ServiceException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+
+    public ServiceException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+
+    public ServiceException(ErrorCode errorCode, String message) {
+        super(message);
+        this.errorCode = errorCode;
+    }
+
+    public ErrorCode getErrorCode() {
+        return this.errorCode;
+    }
+
 }
