11f78d53dfb877ef18e25155b6026a7b7d825456
jin-daram <jindaram1999@gmail.com>
2026-04-23 01:14:37 +0900

    fix: Entity 및 JpaRepository 스캔하도록 개선

--- infra/src/main/java/com/heartbloom/be/infra/config/InfraConfig.java
+++ infra/src/main/java/com/heartbloom/be/infra/config/InfraConfig.java
@@ -1,11 +1,17 @@
 package com.heartbloom.be.infra.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
+@EnableJpaRepositories(basePackages = {"com.heartbloom.be.infra.dao"})
+@EntityScan(basePackages = {"com.heartbloom.be.infra.entity"})
+@EnableJpaAuditing
 public class InfraConfig {
 
     @Bean
