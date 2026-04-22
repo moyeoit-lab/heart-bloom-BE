df34333f13be0e39c918ddd1616b931b87fc9d6e
jin-daram <jindaram1999@gmail.com>
2026-04-22 23:04:35 +0900

    feat: 유저 조회 구현체 추가 (UserReader)

---
+++ app/src/main/java/com/heartbloom/be/app/application/user/implementation/UserReader.java
@@ -0,0 +1,21 @@
+package com.heartbloom.be.app.application.user.implementation;
+
+import com.heartbloom.be.common.exception.ServiceException;
+import com.heartbloom.be.common.exception.UserErrorCode;
+import com.heartbloom.be.core.model.domain.user.User;
+import com.heartbloom.be.core.repository.domain.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class UserReader {
+
+    private final UserRepository userRepository;
+
+    public User read(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new ServiceException(UserErrorCode.NOT_FOUND));
+    }
+
+}
