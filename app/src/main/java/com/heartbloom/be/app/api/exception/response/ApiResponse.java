fd5a1512e5d9a9bfec47b7b8e55e105b6f658ec2
jin-daram <jindaram1999@gmail.com>
2026-04-23 08:42:22 +0900

    fix: ApiResponse 응답 구조 변경
    ApiStatus를 활용하는 것이 아닌 boolean 타입을 반환하도록 변경

--- app/src/main/java/com/heartbloom/be/app/api/exception/response/ApiResponse.java
+++ app/src/main/java/com/heartbloom/be/app/api/exception/response/ApiResponse.java
@@ -1,33 +1,32 @@
 package com.heartbloom.be.app.api.exception.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.heartbloom.be.common.enumerate.ApiStatus;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public record ApiResponse<T> (
-        ApiStatus status,
+        boolean success,
         String message,
         T data
 ) {
 
-    public ApiResponse(ApiStatus status, String message, T data) {
-        this.status = status;
+    public ApiResponse(boolean success, String message, T data) {
+        this.success = success;
         this.message = message;
         this.data = data;
     }
 
     public static <T> ApiResponse<T> success(String message, T data) {
-        return new ApiResponse<>(ApiStatus.SUCCESS, message, data);
+        return new ApiResponse<>(true, message, data);
 
     }
 
     public static <T> ApiResponse<T> success(T data) {
-        return new ApiResponse<>(ApiStatus.SUCCESS, null, data);
+        return new ApiResponse<>(true, null, data);
 
     }
 
     public static <T> ApiResponse<T> fail(String message) {
-        return new ApiResponse<>(ApiStatus.FAIL, message, null);
+        return new ApiResponse<>(false, message, null);
     }
 
 }
