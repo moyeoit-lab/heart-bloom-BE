07238e54161a833586254ff74321e2e250820cc9
jin-daram <jindaram1999@gmail.com>
2026-04-28 18:29:41 +0900

    fix: 꽃다발 진열대 조회 시 senderName 추가

--- app/src/main/java/com/heartbloom/be/app/api/bouquet/response/GetBouquetDisplayStandResponse.java
+++ app/src/main/java/com/heartbloom/be/app/api/bouquet/response/GetBouquetDisplayStandResponse.java
@@ -6,6 +6,7 @@ import java.util.List;
  * 꽃다발 진열대 Response
  */
 public record GetBouquetDisplayStandResponse (
+        String senderName,
         List<GetBouquetResponse> sentBouquets,
         List<GetBouquetResponse> receivedBouquets
 ) {}
